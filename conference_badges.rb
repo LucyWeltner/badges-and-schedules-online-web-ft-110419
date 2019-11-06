c0db4a773dcbee27f5a4d00b19b8f375b9edac5b
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-06 22:32:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  message = "Hello, my name is #{name}."
+  message
+end
+
+def batch_badge_creator(array)
+  badge_array = Array.new 
+  array.each do |name|
+    badge_array << badge_maker(name)
+  end
+  badge_array
+end
+
+def assign_rooms(array)
+  counter = 1 
+  room_assignments = []
+  if array.size =< 7
+    array.each do |name|
+      room_assignments << "Hello, #{name}! You'll be assigned to room #{counter}!"
+        counter += 1
+    end
+    room_assignments
+  end
+end
+  # Write your code here.
